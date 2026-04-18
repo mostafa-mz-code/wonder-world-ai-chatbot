9c253321725bc6ca9417be94bc59e3c7de49869a
mostafa <mostafa.mz.code@gmail.com>
2026-04-18 19:36:09 +0430

    manage environment variables in bun

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,11 +1,13 @@
 import express from "express";
 import type { Request, Response } from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.get("/", (req: Request, res: Response) => {
-  res.send("Hello World");
+  res.send(`Hello World, my api_key ${process.env.OPENAI_API_KEY}`);
 });
 
 app.listen(port, () => {
